68e20b5e7ab8bdec013941aa05970a600a5517fc
Roman Surmanidze <kapa6a3er@gmail.com>
2024-11-04 21:57:21 +0400

    autopush: done template for resource loader

--- loader/src/ILoader.ts
+++ loader/src/ILoader.ts
@@ -1,2 +1,10 @@
+import { LoaderProgressData } from "./LoaderProgressData";
+
 export interface ILoader {
+	amount: number;
+	count: number;
+	progress: LoaderProgressData;
+	isComplete: boolean;
+	load(sourceUrl:string): Promise<any>;
+	get(sourceUrl:string): Promise<any>;
 }
